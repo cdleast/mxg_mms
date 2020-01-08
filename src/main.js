9b7ebbe6d74b96d11101711bec87f759f854e9f4
chendongliang <747439590@qq.com>
2020-01-08 15:00:49 +0800

    安装插件
    ### axios
    * axjx请求数据封装插件
    * cnpm install axios -S
    
    ### pubsub-js
    * 非父子组件通信插件
    * cnpm install pubsub-js -S
    
    ### ElementUI
    * 前端 UI 框架
    * cnpm install element-ui -S
    
    ### vuex
    * vuex用于非父子组件间传值，做数据同步

--- src/main.js
+++ src/main.js
@@ -1,9 +1,21 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui' // ElementUI 组件库
+import 'element-ui/lib/theme-chalk/index.css' // ElementUI 样式
+import axios from 'axios' // axios
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
-Vue.config.productionTip = false
+
+// 全局使用
+Vue.use(ElementUI)
+Vue.prototype.$axios = axios
+
+
+// 开发(false)/生产(true)环境动态切换
+Vue.config.productionTip = process.env.NODE_ENV === 'production'
+// console.log(process.env.NODE_ENV) // 开发换号 development，生产环境 production
+
 
 new Vue({
 	router,
