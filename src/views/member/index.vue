6686718ea0da7c92dbce2294692ff9aac9938567
chendongliang <747439590@qq.com>
2020-01-10 14:49:01 +0800

    新增调用服务接口 API 获取数据
    member.js

--- src/views/member/index.vue
+++ src/views/member/index.vue
@@ -1,12 +1,30 @@
 <template>
-    <div class="member">member</div>
+    <div class="member">
+        {{list}}
+    </div>
 </template>
 
 <script>
+import memberApi from '@/api/member'
 export default {
     name: "member",
     data() {
-        return {};
+        return {
+            list: [], // 获取数据
+
+        };
+    },
+    created(){
+        // 初始化获取数据
+        this.fetchData();
+    },
+    methods:{
+        fetchData(){
+            memberApi.getList().then((res)=>{
+                const resp = res.data;
+                this.list = resp.data;
+            })
+        }
     }
 };
 </script>
